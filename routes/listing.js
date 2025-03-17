ef9e5c58799e63e18dfc210f926f7cb9deb05468
Tushar Pathare <patharetushar10@gmail.com>
2025-03-18 01:20:37 +0530

    Added alert for success using connect-flash

--- routes/listing.js
+++ routes/listing.js
@@ -38,6 +38,7 @@ router.get("/:id", wrapAsync(async (req, res) => {
 router.post("/",validateListing, wrapAsync(async(req, res,next) => {
         const newListing = new Listing(req.body.listing);
         await newListing.save();
+        req.flash("success","New Listing Created");
         res.redirect("/listings"); 
 })
 );
@@ -54,6 +55,7 @@ router.get("/:id/edit", wrapAsync(async (req, res) => {
 router.put("/:id", validateListing,wrapAsync(async (req, res) => {
     const { id } = req.params;
     await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+    req.flash("success","Listing Updated");
     res.redirect(`/listings/${id}`); 
 })
 );
@@ -63,6 +65,7 @@ router.delete("/:id", wrapAsync(async (req,res)=>{
     const { id } = req.params;
     let deletedListing= await Listing.findByIdAndDelete(id);
     console.log("we have deleted it");
+    req.flash("success","Listing Deleted");
     res.redirect("/listings"); 
 })
 );
