3da87183baf155a28bd356710f03334d52d77ae7
Tushar Pathare <patharetushar10@gmail.com>
2025-03-18 01:37:16 +0530

    Added alert for error using connect-flash

--- app.js
+++ app.js
@@ -45,8 +45,10 @@ app.use(flash());
 
 app.use((req,res,next)=>{
      res.locals.success=req.flash("success");
+     res.locals.error=req.flash("error");
      next();
 });
+
 
 app.use("/listings",listings);
 app.use("/listings/:id/reviews",reviews)
