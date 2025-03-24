0aa653022fd41d1acb7947dc4c362c805772f687
Tushar Pathare <patharetushar10@gmail.com>
2025-03-24 20:30:28 +0530

    Refactor code to implement MVC

--- middleware.js
+++ middleware.js
@@ -1,6 +1,7 @@
 const Listing = require("./models/listing");
 const ExpressError=require("./utils/ExpressError.js");
 const { listingSchema,reviewSchema} = require("./schema.js");
+const Review = require("./models/review.js");
 
 module.exports.isLoggedIn = (req, res, next) => {
     if (!req.isAuthenticated()) {
@@ -47,4 +48,14 @@ module.exports.validateReview = (req,res,next)=>{
     }else{
         next();
     }
+};
+
+module.exports.isReviewAuthor = async (req,res,next)=>{
+    let { reviewId ,id} = req.params;
+    let review = await Review.findById(reviewId);
+    if(!review.author.equals(res.locals.currUser._id)){
+        req.flash("error","You are not author of this review");
+        return res.redirect(`/listings/${id}`);
+    }
+    next();
 };
