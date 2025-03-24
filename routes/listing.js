529c027358fe28008a683cca7c3206c06f63de15
Tushar Pathare <patharetushar10@gmail.com>
2025-03-25 01:16:10 +0530

    Set up Cloudinary for image upload

--- routes/listing.js
+++ routes/listing.js
@@ -3,28 +3,30 @@ const router = express.Router();
 const wrapAsync=require("../utils/wrapAsync.js");
 const Listing = require("../models/listing.js");
 const {isLoggedIn,isOwner,validateListing}=require("../middleware.js");
-
 const listingController = require("../controllers/listings.js");
+const multer  = require('multer');
+const {storage}=require("../cloudConfig.js");
+const upload = multer({ storage });
 
-// Index Route
-router.get("/",wrapAsync(listingController.index));
+
+router.route("/")
+  .get(wrapAsync(listingController.index))
+  .post(isLoggedIn,upload.single('listing[image]'),validateListing, wrapAsync(listingController.createListing)
+);
+
+
 
 // New Route
 router.get("/new",isLoggedIn,listingController.renderNewForm);
 
-// Show Route 
-router.get("/:id", wrapAsync(listingController.showListings));
-
-// Create Route 
-router.post("/",isLoggedIn,validateListing, wrapAsync(listingController.createListing));
+router.route("/:id")
+   .get(wrapAsync(listingController.showListings))
+   .put(isLoggedIn,isOwner,validateListing,wrapAsync(listingController.updateListing))
+   .delete(isLoggedIn,isOwner,wrapAsync(listingController.destroyListings)
+);
 
 // Edit Route
 router.get("/:id/edit", isLoggedIn,isOwner,wrapAsync(listingController.renderEditForm));
 
-// Update Route 
-router.put("/:id", isLoggedIn,isOwner,validateListing,wrapAsync(listingController.updateListing));
-
-//Delete route
-router.delete("/:id",isLoggedIn,isOwner,wrapAsync(listingController.destroyListings));
 
 module.exports= router
